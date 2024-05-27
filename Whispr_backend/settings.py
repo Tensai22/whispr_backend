e500855f33adb32f05f19f0251d2d29a73c0cbb2
tensxi <bakooofficial@gmail.com>
2024-05-27 13:07:44 +0500

    Added integration where frontend sends registration data to sqllite.db.

--- Whispr_backend/settings.py
+++ Whispr_backend/settings.py
@@ -82,14 +82,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'auth' : {
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME' : BASE_DIR / 'auth',
-        'USER' : 'root',
-        'PASSWORD' : 'root',
-        'HOST' : 'localhost',
-        'PORT' : '3306'
     }
 }
 
