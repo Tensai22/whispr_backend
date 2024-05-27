499c75ee467e066d7392dec74d1d8d594436b3c3
NurBruh <amdfx9590@bk.ru>
2024-05-27 12:25:07 +0500

    settings.py был добавлен тип судб mysql

--- Whispr_backend/settings.py
+++ Whispr_backend/settings.py
@@ -82,6 +82,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'auth' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : BASE_DIR / 'auth',
+        'USER' : 'root',
+        'PASSWORD' : 'root',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
