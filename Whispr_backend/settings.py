9bbc3b573960422f07db5fa721a6499186f0b3a3
NurBruh <amdfx9590@bk.ru>
2024-05-29 18:04:45 +0500

    Было сменено хостинг сервер базы данных на более ёмкий. Нет необходимости установки python-dotenv. Миграция было выполнено на новый хостинг базы данных. Не забудьде установить Pillow. Все написано в README.md. Пароль от админ панели так же остался.

--- Whispr_backend/settings.py
+++ Whispr_backend/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'Whispr_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
