b8235ec37454b3453d5af3e4bff45df7feb25081
NurBruh <amdfx9590@bk.ru>
2024-06-03 13:30:26 +0500

    Сделал urls чтобы при переходе в профиль пользователя было id/номер в ссылке. Типо того 127.0.0.1:8000/id0

--- logic/urls.py
+++ logic/urls.py
@@ -2,12 +2,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import login_view, register_view, logout_view, profile_update_view, password_change_view, password_reset_view, password_reset_confirm_view, SendMessageView, ReceivedMessagesView
-
+#from .views import *
+from .views import login_view, register_view, logout_view, profile_update_view, password_change_view, password_reset_view, password_reset_confirm_view, SendMessageView, ReceivedMessagesView, profile_view
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('id/<int:id>', profile_view, name='profile'),
     path('profile/update/', profile_update_view.as_view(), name='profile_update'),
     #path('profile/password_change/', password_change_view, name='password_change'),
     path('password_reset/', password_reset_view, name='password_reset'),
