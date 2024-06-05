3dff1ea2c5c9aca633a19fac6630e3904e3c5549
BaUka0 <bauirzhan.bisanov@gmail.com>
2024-06-05 15:25:52 +0500

    исправлены некоторые ошибки

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import Message
+from .models import ChatMessage
 from logic.serializers import UserSerializer
 
 class MessageSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     avatar = serializers.ImageField(source='user.avatar', read_only=True)
     class Meta:
-        model = Message
+        model = ChatMessage
         fields = ['user', 'content', 'timestamp']
